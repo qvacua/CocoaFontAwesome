6d3165096c7fac23e66d8eebded693c5cf13f6fb
Tae Won Ha <h@taewon.de>
2016-11-27 20:42:26 +0100

    Initial Commit

---
+++ CocoaFontAwesome/CocoaFontAwesome.h
@@ -0,0 +1,19 @@
+//
+//  CocoaFontAwesome.h
+//  CocoaFontAwesome
+//
+//  Created by Tae Won Ha on 27/11/2016.
+//  Copyright © 2016 Tae Won Ha. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaFontAwesome.
+FOUNDATION_EXPORT double CocoaFontAwesomeVersionNumber;
+
+//! Project version string for CocoaFontAwesome.
+FOUNDATION_EXPORT const unsigned char CocoaFontAwesomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaFontAwesome/PublicHeader.h>
+
+
