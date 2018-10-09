f54ff4d5f396f598d8761ed493323aaddf02a2c8
chriszielinski <chrisz@berkeley.edu>
2018-10-08 18:49:06 -0700

    Update to most recent FontAwesome.swift

--- CocoaFontAwesomeDemo/AppDelegate.swift
+++ CocoaFontAwesomeDemo/AppDelegate.swift
@@ -12,34 +12,36 @@ import CocoaFontAwesome
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var window: NSWindow!
 
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    let contentView = self.window.contentView!
-    let image = NSImage.fontAwesomeIcon(code: "fa-times-circle",
-                                        textColor: .darkGray,
-                                        dimension: 16)!
-    let button = NSButton(frame: CGRect(x: 50, y: 50, width: 40, height: 40))
-    button.imagePosition = .imageOnly
-    button.image = image
-    button.isBordered = false
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentView = self.window.contentView!
+        let image = NSImage.fontAwesomeIcon(code: "fa-times-circle",
+                                            style: .regular,
+                                            textColor: .darkGray,
+                                            dimension: 16)
+        let button = NSButton(frame: CGRect(x: 50, y: 50, width: 40, height: 40))
+        button.imagePosition = .imageOnly
+        button.image = image
+        button.isBordered = false
 
-    // The following disables the square appearing when pushed.
-    let cell = button.cell as? NSButtonCell
-    cell?.highlightsBy = .contentsCellMask
-    cell?.backgroundColor = .yellow
+        // The following disables the square appearing when pushed.
+        let cell = button.cell as? NSButtonCell
+        cell?.highlightsBy = .contentsCellMask
+        cell?.backgroundColor = .yellow
 
-    contentView.addSubview(button)
-  }
+        contentView.addSubview(button)
+    }
 }
 
 class Dummy: NSView {
 
-  let image = NSImage.fontAwesomeIcon(name: .close,
-                                      textColor: .black,
-                                      dimension: 18)
+    let image = NSImage.fontAwesomeIcon(name: .windowClose,
+                                        style: .regular,
+                                        textColor: .black,
+                                        dimension: 18)
 
-  override func draw(_ dirtyRect: NSRect) {
-    image.draw(in: CGRect(x: 2, y: 2, width: 18, height: 18))
-  }
+    override func draw(_ dirtyRect: NSRect) {
+        image.draw(in: CGRect(x: 2, y: 2, width: 18, height: 18))
+    }
 }
